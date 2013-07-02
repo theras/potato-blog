05608bbfa03406113cbe16d907c788084a7bf2e4
Lumina <scott.a.raine@gmail.com>
2013-07-02 14:39:52 +0100

    Added first request factory test

--- core/tests/admin_tests.py
+++ core/tests/admin_tests.py
@@ -5,12 +5,13 @@ from google.appengine.ext import ndb
 from google.appengine.ext import testbed
 
 from ndbtestcase import NdbTestCase
-
 from core.models import Post
+from core.views.admin_views import *
+from django.test.client import RequestFactory
 
 
 class AdminViewsTestCase(NdbTestCase):
-	def testAddPost(self):
+	def test_add_post(self):
 		post = Post(
 			title = 'Test Post',
 			brief = 'To infinity ... and beyond.',
@@ -23,6 +24,13 @@ class AdminViewsTestCase(NdbTestCase):
 		post_title = post_key.get().title
 		
 		self.assertEqual(post_title, post.title)
+		
+	def test_presence_of_add_post_page(self):
+		self.factory = RequestFactory()
+		request = self.factory.get('/admin/add/')
+		response = admin_post_add(request)
+		
+		self.assertEqual(response.status_code, 200)
 	
 if __name__ == '__main__':
 	unittest.main()
